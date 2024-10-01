aecd6a0b57349cea7a0d44aa5daaea729015af56
Vivek Pandey <101510298+vickypandey14@users.noreply.github.com>
2024-10-01 23:53:27 +0530

    Update change-password.blade.php

--- resources/views/backend/change-password.blade.php
+++ resources/views/backend/change-password.blade.php
@@ -6,7 +6,7 @@
 
 <div class="page-content">
     <div class="container-fluid">
-        <!-- start page title -->
+        
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
@@ -14,7 +14,6 @@
                 </div>
             </div>
         </div>
-        <!-- end page title -->
 
         <div class="row">
 
