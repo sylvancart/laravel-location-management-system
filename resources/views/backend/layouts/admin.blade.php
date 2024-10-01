6ef04df3da59815cf48a8b79ede30870d41699ec
Vivek Pandey <101510298+vickypandey14@users.noreply.github.com>
2024-10-01 23:56:06 +0530

    Update admin.blade.php

--- resources/views/backend/layouts/admin.blade.php
+++ resources/views/backend/layouts/admin.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         
-        <!-- App favicon -->
+       
         <link rel="icon" href="{{ asset('admin/images/favicon.ico') }}" />
 
         <!-- Bootstrap Css -->
